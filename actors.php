ce5eecb62ed0b2ad1277bd9d63a5854f7248a663
andrei674427 <andrejboldyrev913@gmail.com>
2026-04-13 08:16:17 +0500

    Update and rename characters.html to actors.php

---
+++ actors.php
@@ -0,0 +1,30 @@
+<?php include "includes/header.php"; ?>
+<link rel="stylesheet" href="css/style.css">
+
+<section class="page">
+    <h1>Актёры</h1>
+    <div class="actors-grid">
+        <div class="actor-card">
+            <img src="/images/kolyan.jpg" alt="Николай Наумов">
+            <h3>Николай Наумов</h3>
+            <p>Главный герой сериала, друг Антохи, Вована и Эдика.</p>
+        </div>
+        <div class="actor-card">
+            <img src="/images/antokha.jpeg" alt="Антон Богданов">
+            <h3>Антон Богданов</h3>
+            <p>Друг Коляна и Вована, зачастую ругающий их за малейший «косяк». Неудачлив‚ однако смекалист. </p>
+        </div>
+        <div class="actor-card">
+            <img src="/images/lera.jpg" alt="Зоя Бербер">
+            <h3>Зоя Бербер</h3>
+            <p>Девушка из обеспеченной семьи. Ничего из себя не представляет без папиных денег, безуспешно пытается доказать обратное. </p>
+        </div>
+        <div class="actor-card">
+            <img src="/images/vovan.jpg" alt="Владимир Селиванов">
+            <h3>Владимир Селиванов</h3>
+            <p>Главный герой сериала, друг Коляна и Антохи. Глуповат, при этом добрый, весёлый и бесхитростный.</p>
+        </div>
+    </div>
+</section>
+
+<?php include "includes/footer.php"; ?>
